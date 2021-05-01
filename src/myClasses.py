bf3e97e9935f41d562a77e092a8833b2dbd4279e
PhiCtl <philippine.descourtils@epfl.ch>
2021-05-01 14:18:01 +0200

    corrected typo in area

--- src/myClasses.py
+++ src/myClasses.py
@@ -137,7 +137,8 @@ class FlowerMaskDetectionDataset(torch.utils.data.Dataset):
 
         # there is only one class (either background either flower)
         labels = torch.ones((len(bboxes),), dtype=torch.int64)
-        area = torch.abs((bboxes[:, 2] - bboxes[:, 0])*(bboxes[:, 3] - bboxes[:, 1]))
+        area = torch.as_tensor((bboxes[:, 2] - bboxes[:, 0])*(bboxes[:, 3] - bboxes[:, 1]), dtype=torch.float32)
+        area = torch.abs(area)
         iscrowd = torch.zeros((len(bboxes),), dtype=torch.int64) # all instances are not crowd (?!) # TODO what is iscrowd
 
         # Prepare sample
