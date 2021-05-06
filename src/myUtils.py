71f224bea36eec6209a47cc1b3beb2f37727114c
PhiCtl <44364660+PhiCtl@users.noreply.github.com>
2021-05-06 16:19:02 +0200

    Added get model function

--- src/myUtils.py
+++ src/myUtils.py
@@ -4,6 +4,10 @@ import numpy as np
 import torchvision.transforms as T
 import os
 from torchvision.models.detection.transform import GeneralizedRCNNTransform
+from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 import torch.functional as F
 #from google.colab.patches import cv2_imshow
 
@@ -193,6 +197,52 @@ def eval_custom(dataset, model, device=device):
         predictions.append(p)
 
   return predictions
+
+
+def get_object_detection_model(num_classes, mtype = 'Resnet50_FPN'):
+    # load a model pre-trained on COCO
+
+    if mtype ==  'Resnet50_FPN':
+      model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+      # get the number of input features for the classifier
+      in_features = model.roi_heads.box_predictor.cls_score.in_features
+      # replace the pre-trained head with a new one
+      model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    if mtype == 'MobileNetV3_largeFPN':
+
+      model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
+      # get the number of input features for the classifier
+      in_features = model.roi_heads.box_predictor.cls_score.in_features
+      # replace the pre-trained head with a new one
+      model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    
+    if mtype == 'MobileNetV3_largeFPN_320':
+      # Low resolution network
+      model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
+      # get the number of input features for the classifier
+      in_features = model.roi_heads.box_predictor.cls_score.in_features
+      # replace the pre-trained head with a new one
+      model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    if mtype == 'MaskRCNN':
+      # load an instance segmentation model pre-trained on COCO
+      model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+      # get the number of input features for the classifier
+      in_features = model.roi_heads.box_predictor.cls_score.in_features
+      # replace the pre-trained head with a new one
+      model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+      # now get the number of input features for the mask classifier
+      in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+      hidden_layer = 256
+      # and replace the mask predictor with a new one
+      model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                          hidden_layer,
+                                                          num_classes)
+
+    return model
 
 
 
